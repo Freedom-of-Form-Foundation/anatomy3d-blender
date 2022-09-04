51d20a73141d6eb96a97e4e57a47ca8ba3faba69
Lathreas <lathreas@protonmail.com>
2022-09-04 19:12:55 -0400

    Add Boolean type

--- addons/geoscript/types/__init__.py
+++ addons/geoscript/types/__init__.py
@@ -2,8 +2,8 @@
 
 from .abstract_socket import AbstractSocket
 from .abstract_tensor import AbstractTensor
+from .boolean import Boolean
 from .scalar import Scalar
 from .vector3 import Vector3
 
-__all__ = ["AbstractSocket", "AbstractTensor", "Scalar", "Vector3"]
-
+__all__ = ["AbstractSocket", "AbstractTensor", "Boolean", "Scalar", "Vector3"]
