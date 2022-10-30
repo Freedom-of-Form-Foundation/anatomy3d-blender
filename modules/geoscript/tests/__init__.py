f4dd8c5e3b4306dc6ede0ecf9f8f82ae18b5b5ca
Lathreas <72281292+Lathreas@users.noreply.github.com>
2022-10-30 08:35:23 -0400

    [CI] Add workflow for pytest-blender unit tests (#3)
    * Add some very simple unit tests
    
    * [CI] Add workflow for pytest-blender
    
    * Add some unit tests for math.py

---
+++ modules/geoscript/tests/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+"""This package contains unit tests for Geoscript"""
+
+# This file intentionally contains no code. See the other .py files in this package
+# for the specific unit tests.
