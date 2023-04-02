f635c9f53739704d5efbd2a58091a5a823e830b6
Pine <eli.stonium@gmail.com>
2023-04-02 17:41:57 -0400

    Custom Edit Context Views (#4)
    * Custom Edit Context Views
    
    Adds custom edit context views and closes #7
    
    The views added are "Armature Mode", "Skeleton Mode", "Muscle Mode", and "Skin Mode"
    
    Additionally, there's a "Smart Object Mode" which functions as "Bone Edit Context" would but can be run on any object, not just bones.
    
    Smart Object Mode will make visible any selected objects, plus any objects in any geometry nodes present on these selected objects.
    
    * Smart Object Select now relies on geometry_constraints
    
    as explicitly set in the ARF menu rather than trying to intuit them from the geometry nodes.
    
    Also adds asserts for ARF existing, removes any code going over line 80, and adds doc strings to the
    top of the file.

--- scripts/ui/__init__.py
+++ scripts/ui/__init__.py
@@ -3,13 +3,15 @@
 """Contains all user interface elements for the Blender plugin."""
 
 from . import bone_properties
-
+from . import mode_selection
 
 def register_all():
     bone_properties.register()
+    mode_selection.register()
     print("Registered scripts.ui")
 
 
 def unregister_all():
     bone_properties.unregister()
+    mode_selection.unregister()
     print("Unregistered scripts.ui")
